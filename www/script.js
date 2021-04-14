026b3d3160e01c606ea5319f9def81b7148af499
cisamu123 <76702667+cisamu123@users.noreply.github.com>
2021-04-14 19:26:20 +0300

    Add files via upload

---
+++ www/script.js
@@ -0,0 +1,33 @@
+var fly = new Audio();
+var menu = new Audio();
+var gameower = new Audio();
+
+fly.src = "audio/play.mp3";
+menu.src = "audio/menu.mp3";
+
+
+var character = document.getElementById("character");
+var block = document.getElementById("block");
+var counter=0;
+function jump(){
+    if(character.classList == "animate"){return}
+    character.classList.add("animate");
+    setTimeout(function(){
+        character.classList.remove("animate");
+    },300);
+
+   
+}
+var checkDead = setInterval(function() {
+    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
+        block.style.animation = "none";
+        alert("Game Over. score: "+Math.floor(counter/100));
+        counter=0;
+        block.style.animation = "block 1s infinite linear";
+    }else{
+        counter++;
+        document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
+    }
+}, 10);
